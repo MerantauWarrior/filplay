53b43c529f2b154c5b642d5563df491858c19204
merantauwarrior <boris1488ss@gmail.com>
2023-04-11 18:39:22 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "black"};
+  background: white;
+  font-size: 24px;
+  border: 2px solid black;
+  border-radius: ${props => props.bdRadius || 0};
+`;
+
+const Button = styled.button`
+width: 200px;
+font-size: 30px;
+background: ${props => props.bg || "white"};
+color: ${props => props.$primary ? "black" : "palevioletred"};
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>test</h1>
+<Button bg="purple">Primary</Button>
+<Button $primary>Primary</Button>
+<Input type="text" inputColor="rebeccapurple" bdRadius="10px" />
     </div>
   );
 }
